375a2c2f5eddef56f997f824b4f790f52bb7029b
Szymon Polaczy <thomas_frey@tutanota.com>
2024-09-04 01:17:57 +0200

    Running forward blind

--- server.go
+++ server.go
@@ -5,11 +5,12 @@ import (
 	"github.com/labstack/echo/v4"
 	"fmt"
     "io/ioutil"
+    "encoding/json"
 )
 
 type Task struct {
-	ID int `json:"task_id"`
-	Name string `json:"name"`
+	task_id int `json:"task_id"`
+	name string `json:"name"`
 }
 
 func main() {
@@ -17,7 +18,7 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		timecamp_api_key := "cb27cd4c3598f624e85309c40c"
 		server_url := "https://app.timecamp.com/third_party/api" //mock server url
-		tasks_url := server_url + "/tasks"
+		tasks_url := server_url + "/tasks/168140512"
 
 		// Create a Bearer string by appending string access token
 		bearer := "Bearer " + timecamp_api_key
@@ -45,6 +46,38 @@ func main() {
 		if err != nil {
 			fmt.Printf("error reading tasks request")
 		}
+
+		//var result map[string]interface{}
+		var result map[string]map[string]Task
+
+		err = json.Unmarshal([]byte(body), &result)
+
+		if err != nil {
+			// print out if error is not nil
+			fmt.Println(err)
+		}
+
+		fmt.Println(result)
+		// printing details of map
+		// iterate through the map
+		for key, value := range result {
+			fmt.Println(key, ":", value)
+		}
+
+		/*var tasks []Task
+		err = json.Unmarshal([]byte(body), &tasks)
+
+		if err != nil {
+			fmt.Println("JSON decode error!")
+		}
+
+		fmt.Println(tasks)
+
+		for key, value := range tasks {
+			fmt.Println("Key:", key, "Value:", value)
+		}*/
+
+		//fmt.Println("Price of the second product:", objMap[1]["name"])
 
 		return c.String(http.StatusOK, string(body))
 	})
