d5b3fa278b11e2206628d1b8799e039766c87a64
Szymon Polaczy <thomas_frey@tutanota.com>
2024-10-21 23:17:04 +0200

    Get tasks from database

--- server.go
+++ server.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Page struct {
@@ -29,6 +32,40 @@ func main() {
 		t, _ := template.ParseFiles("homepage.html")
 
 		t.Execute(w, p)
+	})
+
+	http.HandleFunc("/sync", func(w http.ResponseWriter, r *http.Request) {
+		//connect with sql database, then connect with an api to get data and put it in the database
+		//then return the new data to the user
+
+		db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/oye")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer db.Close()
+
+		err = db.Ping()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rows, err := db.Query("SELECT * FROM tasks")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer rows.Close()
+
+		for rows.Next() {
+			var id int
+			var name string
+			err = rows.Scan(&id, &name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println(id, name)
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":1234", nil))
