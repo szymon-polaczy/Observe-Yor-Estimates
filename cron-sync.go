13513c529e83e4ca4640dd317f481c4851aeba74
Szymon <thomas_frey@tutanota.com>
2025-04-20 21:24:41 +0200

    Add proper connection with timecamp; Getting all of the tasks from timecamp

---
+++ cron-sync.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Task struct {
+	TaskID      int    `json:"task_id"`
+	ParentID    int    `json:"parent_id"`
+	AssignedBy  int    `json:"assigned_by"`
+	Name        string `json:"name"`
+	Level       int    `json:"level"`
+	BudgetUnit  string `json:"budget_unit"`
+	RootGroupID int    `json:"root_group_id"`
+}
+
+func main() {
+	// contact timecamp and get the tasks
+	// open a connection with the database - do we create it using an outside thing or do we add it's creation here?
+	// how do we update everythign - if we would actually be getting a full database each time then we can basically override it and add data from the start
+	// if we don't get everything then we would have to update things where most of the things don't change
+	// so we already know that we have to at least get all of the json, run it through a hash and see what value we get - if we already synced that value then we don't have
+	// anything more to sync and we can stop - with the option to override for testing
+
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	timecamp_api_url := "https://app.timecamp.com/third_party/api"
+	get_all_tasks_url := timecamp_api_url + "/tasks"
+
+	auth_bearer := "Bearer " + os.Getenv("TIMECAMP_API_KEY")
+
+	request, err := http.NewRequest("GET", get_all_tasks_url, nil)
+	request.Header.Add("Authorization", auth_bearer)
+	request.Header.Add("Accept", "application/json")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Unmarshal into a map first
+	taskMap := make(map[string]Task)
+	if err := json.Unmarshal(body, &taskMap); err != nil {
+		panic(err)
+	}
+
+	// Convert the map to a slice
+	tasks := make([]Task, 0, len(taskMap))
+	for _, task := range taskMap {
+		tasks = append(tasks, task)
+	}
+
+	// Output the slice to check
+	for _, t := range tasks {
+		fmt.Printf("%+v\n", t)
+	}
+}
