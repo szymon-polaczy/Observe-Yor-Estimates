bc2afa439c4a0a96f0a07731f15bb50ed03c4eff
Szymon <thomas_frey@tutanota.com>
2025-04-21 14:10:50 +0200

    Server now gets the saved tasks and returns them in json via an api

--- server.go
+++ server.go
@@ -1,168 +1,70 @@
 package main
 
 import (
-	"bytes"
+	"database/sql"
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strconv"
-	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-// "github.com/labstack/echo/v4"
-type SimpleTask struct {
-	Task_id     string `json:"task_id"`
+type tasksHandler struct{}
+
+type JsonTask struct {
+	TaskID      int    `json:"task_id"`
+	ParentID    int    `json:"parent_id"`
+	AssignedBy  int    `json:"assigned_by"`
 	Name        string `json:"name"`
-	Assigned_by string `json:"assigned_by"`
-}
-
-type Tasks struct {
-	TaskIds   []string `json:"task_ids"`
-	StartDate string   `json:"startDate"`
-	EndDate   string   `json:"endDate"`
-}
-
-type Duration struct {
-	Duration int `json:"duration"`
-}
-
-func GetStringInBetween(str string, start string, end string) (result string) {
-	s := strings.Index(str, start)
-	if s == -1 {
-		return
-	}
-	s += len(start)
-	e := strings.Index(str[s:], end)
-	if e == -1 {
-		return
-	}
-	e += s + e - 4 //QUICK FIx
-	return str[s:e]
+	Level       int    `json:"level"`
+	RootGroupID int    `json:"root_group_id"`
 }
 
 func main() {
-	/*e := echo.New()
-	e.GET("/", func(c echo.Context) error {*/
-	timecamp_api_key := "cb27cd4c3598f624e85309c40c"
-	server_url := "https://app.timecamp.com/third_party/api" //mock server url
-	tasks_url := server_url + "/tasks/168140512"
+	request_multiplexer := http.NewServeMux()
 
-	// Create a Bearer string by appending string access token
-	bearer := "Bearer " + timecamp_api_key
+	request_multiplexer.Handle("/tasks", &tasksHandler{})
 
-	// Create a new request using http
-	req, err := http.NewRequest("GET", tasks_url, nil)
+	http.ListenAndServe(":9238", request_multiplexer)
+}
 
-	// add authorization header to the req
-	req.Header.Add("Authorization", bearer)
-	req.Header.Add("Accept", "application/json")
+func (t *tasksHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	switch {
+	case request.Method == http.MethodGet:
+		t.GetTasks(response, request)
+		return
+	default:
+		return
+	}
+}
 
-	// Send req using http Client
-	client := &http.Client{}
-	resp, err := client.Do(req)
-
+func (t *tasksHandler) GetTasks(response http.ResponseWriter, request *http.Request) {
+	db, err := sql.Open("sqlite3", "./oye.db")
 	if err != nil {
-		fmt.Printf("error making tasks request")
+		panic(err)
 	}
-	defer resp.Body.Close()
+	defer db.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-
+	select_all_tasks_sql := `SELECT * FROM tasks;`
+	db_tasks, err := db.Query(select_all_tasks_sql)
 	if err != nil {
-		fmt.Printf("error reading tasks request")
+		panic(err)
 	}
 
-	x := make(map[string]SimpleTask)
-	err = json.Unmarshal([]byte(body), &x)
-	if err := json.Unmarshal([]byte(body), &x); err != nil {
-		fmt.Println("error")
-	}
-	//fmt.Println(x)
-
-	for key, value := range x {
-
-		fmt.Println("Task ID:", key)
-		fmt.Println("Name:", value.Name)
-
-		time_estimated := GetStringInBetween(value.Name, "[", "]")
-		fmt.Println(time_estimated, "time estimated in hours")
-
-		hours := strings.Split(time_estimated, "-")
-		fmt.Println(hours)
-
-		//if !strings.Contains(value.Name, "[") || !strings.Contains(value.Name, "]") {
-		tasks := Tasks{
-			TaskIds:   []string{key},
-			StartDate: "2024-09-01",
-			EndDate:   "2024-09-15",
-		}
-		marshalled, err := json.Marshal(tasks)
-
-		fmt.Println(string(marshalled))
-
+	tasks_list := []JsonTask{}
+	for db_tasks.Next() {
+		task := JsonTask{}
+		err = db_tasks.Scan(&task.TaskID, &task.ParentID, &task.AssignedBy, &task.Name, &task.Level, &task.RootGroupID)
 		if err != nil {
-			fmt.Println("error marshalling")
-		}
-
-		request_url := "https://app.timecamp.com/third_party/api/v3/time-entries"
-		// Create a new request using http
-		req, err := http.NewRequest("POST", request_url, bytes.NewReader(marshalled))
-		// add authorization header to the req
-		req.Header.Add("Authorization", bearer)
-		req.Header.Add("Accept", "application/json")
-		req.Header.Add("Content-Type", "application/json")
-		// Add request body
-		//req.Body = ioutil.NopCloser(strings.NewReader(request_body))
-		// Send req using http Client
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Printf("error making time entry request")
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Printf("error reading time entry request")
-		}
-		fmt.Println(string(body))
-
-		x := []Duration{}
-		err = json.Unmarshal([]byte(body), &x)
-		if err := json.Unmarshal([]byte(body), &x); err != nil {
-			fmt.Println("error")
-		}
-		fmt.Println(x)
-
-		total_time_spent := 0
-		for _, value := range x {
-			fmt.Println(value.Duration, "seconds")
-			total_time_spent += value.Duration
-		}
-
-		fmt.Println(total_time_spent, "total time spent in seconds")
-
-		min_hours, err := strconv.Atoi(hours[0])
-		if err != nil {
-			// ... handle error
 			panic(err)
 		}
-		max_hours, err := strconv.Atoi(hours[1])
-		if err != nil {
-			// ... handle error
-			panic(err)
-		}
-
-		var min_percentage float32 = float32(float32(total_time_spent) / float32(min_hours*3600) * float32(100.3))
-		var max_percentage float32 = float32(float32(total_time_spent) / float32(max_hours*3600) * float32(100.0))
-
-		fmt.Printf("%.2f Percentage of the minimal estimation\r\n", min_percentage)
-		fmt.Printf("%.2f Percentage of the maximal estimation\r\n\r\n", max_percentage)
-		//}
-
+		tasks_list = append(tasks_list, task)
 	}
 
-	/*return c.String(http.StatusOK, string(body))
-	})
-	e.Logger.Fatal(e.Start(":1324"))*/
+	json_tasks, err := json.Marshal(tasks_list)
+	if err != nil {
+		panic(err)
+	}
+
+	response.WriteHeader(http.StatusOK)
+	response.Write(json_tasks)
 }
